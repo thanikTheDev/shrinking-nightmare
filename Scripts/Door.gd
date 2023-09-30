01f3cb4bb7577c730690e4bf320ca5bd72feb444
thanikTheDev <thanik1234@gmail.com>
2023-09-30 13:15:10 -0400

    Rename SwitchScenes to Door and Added Key Support

---
+++ Scripts/Door.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+@export_file("*.tscn") var next_level
+@export var key : Node2D
+
+var locked : bool = false
+
+func _ready():
+	if key:
+		locked = true
+		key.collected.connect(_on_key_collected)
+
+func _on_body_entered(body):
+	if body.is_in_group("Player") and not locked:
+		get_tree().change_scene_to_file(next_level)
+
+func _on_key_collected():
+	locked = false
