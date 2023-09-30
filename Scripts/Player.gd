3dd479bb2497522592de67aa5e8e476f57051896
thanikTheDev <thanik1234@gmail.com>
2023-09-30 15:39:52 -0400

    Added Crouch

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,28 +1,43 @@
 extends CharacterBody2D
 
-const SPEED = 150.0
-const JUMP_VELOCITY = -400.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var direction : float
-
 signal moved()
+
+const JUMP_VELOCITY = -400.0
+const SPEED = 150.0
+
+@onready var collider : CollisionShape2D = $Collider
+var crouched : bool = false
+var direction : float
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
 	var previous_position : Vector2 = global_position
+	var current_speed = SPEED
 	
-	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+	else:
+		if Input.is_action_pressed("crouch") and not crouched:
+			crouched = true
+			current_speed *= 0.5
+			collider.shape.height = 15
+			collider.position.y = 8
+		elif crouched:
+			current_speed *= 0.5
+			if not Input.is_action_pressed("crouch") and not is_under_object():
+				crouched = false
+		else:
+			crouched = false
+			collider.shape.height = 31
+			collider.position.y = 0
+			
 
-	# Handle Jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	direction = Input.get_axis("left", "right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * current_speed
 	else:
 		velocity.x = 0
 
@@ -33,3 +48,9 @@ func _physics_process(delta):
 
 func game_over():
 	get_tree().reload_current_scene()
+
+func is_under_object():
+	var space_state = get_world_2d().direct_space_state
+	var query = PhysicsRayQueryParameters2D.create(global_position, global_position + Vector2.UP)
+	query.exclude = [self]
+	return space_state.intersect_ray(query)
