4ef5cc1e3e3be28e513ab63988d2b37e464bcbbf
thanikTheDev <thanik1234@gmail.com>
2023-10-02 11:41:27 -0400

    Added DeathZone Prefab and Animation

--- Scripts/DeathZone.gd
+++ Scripts/DeathZone.gd
@@ -3,8 +3,11 @@ extends Area2D
 @export var direction : Vector2
 @export var speed : float = 1
 
+@onready var _audio = $AudioStreamPlayer2D
+
 func _on_player_moved():
 	global_position += direction * speed
+	_audio.play(3.4)
 
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
